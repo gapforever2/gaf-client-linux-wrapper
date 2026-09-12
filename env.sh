44a66d55935533289b1666327f5c5647ed9e31a1
vladik8888 <vladiab3@gmail.com>
2026-09-12 16:19:42 +0300

    added warning

--- env.sh
+++ env.sh
@@ -1,8 +1,12 @@
 export DATA_DIR="$HOME/.local/share/gaf-client-legacy"
 
+# ------------------------------------------------------------------------
+# Internal paths changing may cause issues when running multiple instances
+# We recommend changing only DATA_DIR path if you want to move data folders
 export PROTONPATH="$DATA_DIR/proton"
 export WINEPREFIX="$DATA_DIR/prefix"
 export XDG_CACHE_HOME="$DATA_DIR/cache"
+# ------------------------------------------------------------------------
 
 export PROTON_LOG=0
 export WINEDEBUG="-all"
