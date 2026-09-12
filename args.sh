112da14557e1451c1ee15829e9b08b85e65484e7
vladik8888 <vladiab3@gmail.com>
2026-09-12 15:47:46 +0300

    some fixes

--- args.sh
+++ args.sh
@@ -3,7 +3,7 @@ if [ -z "$1" ] || [ ! -f "$1" ] || [ "$(basename "$1")" != "gaf-client.exe" ]; t
         --text="Исполняемый файл указан неверно. Проверьте параметры запуска." \
         --button="ОК:0" \
         --width=350 \
-        --height=120 \
+        --height=80 \
         --fixed \
         --center \
         --justify=center
