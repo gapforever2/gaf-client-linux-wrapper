d78426c303ee1c39c20e4c32f68b09da24dff94d
vladik8888 <vladiab3@gmail.com>
2026-09-13 13:35:18 +0300

    fix sfx-update error

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,7 @@ mkdir -p "$PROTONPATH"
 mkdir -p "$WINEPREFIX"
 mkdir -p "$XDG_CACHE_HOME"
 
-PROTON_URL="https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-34/GE-Proton10-34.tar.gz"
+PROTON_URL="https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-32/GE-Proton10-32.tar.gz"
 
 PROTON_FILE="${PROTON_URL##*/}" 
 PROTON_TMP="$XDG_CACHE_HOME/$PROTON_FILE"
