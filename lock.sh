5502dc96bfd4bdac7e002741ea58f8643367e53c
vladik8888 <vladiab3@gmail.com>
2026-09-18 17:18:46 +0300

    already launched text reworked

--- lock.sh
+++ lock.sh
@@ -3,10 +3,10 @@ if [ -e "$LOCKFILE" ]; then
     PID=$(cat "$LOCKFILE" 2>/dev/null)
     if [ -n "$PID" ] && kill -0 "$PID" 2>/dev/null; then
         yad --title="Gaf Client: Legacy" \
-            --text="Приложение уже запущено! Повторный запуск невозможен.\nЧтобы запускать несколько экземпляров одновременно,\nзадайте другой DATA_DIR в user_env.sh скрипте." \
+            --text="Приложение уже запущено! Повторный запуск невозможен."\
             --button="ОК:0" \
             --width=350 \
-            --height=120 \
+            --height=80 \
             --fixed \
             --center \
             --justify=center
